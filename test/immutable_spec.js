c2f54f2a1a19da7bad003039114b5d18af315ee5
Francisco De La Cruz <helloquico@gmail.com>
2015-10-16 13:37:05 -0400

    Immutable Lists Test

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,3 +1,4 @@
+import {List} from 'immutable';
 import {expect} from 'chai';
 
 describe('immutability', () => {
@@ -17,5 +18,27 @@ describe('immutability', () => {
     });
 
   });
+
+	describe('A List', () => {
+		function addMovie(currentState, movie) {
+			return currentState.push(movie);
+		}
+
+		it('is immutable', () => {
+			let state = List.of('Trainspotting', '28 Days Later');
+			let nextState = addMovie(state, 'Sunshine');
+
+			expect(nextState).to.equal(List.of(
+				'Trainspotting',
+				'28 Days Later',
+				'Sunshine'
+			));
+			expect(state).to.equal(List.of(
+				'Trainspotting',
+				'28 Days Later'
+			));
+		});
+
+	});
 
 });
